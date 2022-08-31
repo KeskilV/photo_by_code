b73cadd81ef31476544c653b0ac1e21580020587
KeskilV <gemlab@mail.ru>
2022-08-31 16:19:11 +0900

    add pngs

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 
-decoded = decode(Image.open('img/IMG_6036.JPG'))
+decoded = decode(Image.open('1200434418110830.NgZ9ZTaRjFlRHpZnRh2z_height640.png'))#'img/817d189b-9f3e-4794-a76b-a73307bbaf4e.jpg'))
 print(decoded[0].data.decode("utf-8"))
